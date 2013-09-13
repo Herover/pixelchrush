7c38c7ae490aff5d72fdeaec3c5dcaaa56d9e16e
Herover <leon.l.a.nielsen@gmail.com>
2013-09-13 11:55:46 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+    "name": "pixelchrush",
+    "version": "1.0.0",
+    "description": "Abstract 2D multiplayer game",
+    "dependencies": {
+        "socket.io": "0.9.14"
+    },
+    "author": "Herover",
+    "license": "MIT",
+    "repository": {
+    }
+}
